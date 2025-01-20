0cda386a862b9a9a9a317d42888e13e3bfd1ad8e
Robson Oliveira dos Santos <robsonos@outlook.com>
2025-01-20 14:10:41 +0930

    build(repo): simplify release process

--- nxrelease.config.js
+++ nxrelease.config.js
@@ -98,7 +98,7 @@ module.exports = {
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
-    'next',
+    { name: 'next', prerelease: true },
     { name: 'beta', prerelease: true },
     { name: 'alpha', prerelease: true },
   ],
