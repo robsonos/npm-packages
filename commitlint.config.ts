9d09cb97f81edd6305237261cfab3dc385344b71
Robson Oliveira dos Santos <robsonos@outlook.com>
2025-01-22 01:13:12 +0930

    build: remove commit lint rules in favour of nx-semantic-release style  [skip ci]
    [skip all]

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,14 +1,7 @@
 import type { UserConfig } from '@commitlint/types';
-import { RuleConfigSeverity } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    // Enforces specific scopes
-    'scope-enum': [RuleConfigSeverity.Error, 'always', ['repo', 'ionic-mdi']],
-    // Ensures that a scope is always provided
-    'scope-empty': [RuleConfigSeverity.Error, 'never'],
-  },
 };
 
 export default Configuration;
